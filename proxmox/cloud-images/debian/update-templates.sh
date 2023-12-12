02af09c71f91a1e4b8f9b6c4cd1beb7d462463ee
NathanWarrick <nathan@wimmerait.com.au>
2023-12-12 23:22:10 +1100

    updated update-all scripts

---
+++ proxmox/cloud-images/debian/update-templates.sh
@@ -0,0 +1,3 @@
+bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Boilerplates/main/proxmox/cloud-images/debian/debian-10.sh)"
+bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Boilerplates/main/proxmox/cloud-images/debian/debian-11.sh)"
+bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Boilerplates/main/proxmox/cloud-images/debian/debian-12.sh)"
---
+++ proxmox/cloud-images/update-all.sh
@@ -0,0 +1,4 @@
+bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Boilerplates/main/proxmox/cloud-images/ubuntu/update-images.sh)"
+bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Boilerplates/main/proxmox/cloud-images/ubuntu/update-templates.sh)"
+bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Boilerplates/main/proxmox/cloud-images/debian/update-templates.sh)"
+
