f966238d1854914d1c78a854d38e092187c4e4c3
NathanWarrick <nathan@wimmerait.com.au>
2023-12-13 12:30:16 +1100

    chaned alpine to 9030

--- proxmox/cloud-images/alpine/alpine.sh
+++ proxmox/cloud-images/alpine/alpine.sh
@@ -1,16 +1,16 @@
-qm destroy 9020
+qm destroy 9030
 wget -c https://dl-cdn.alpinelinux.org/alpine/v3.19/releases/x86_64/alpine-virt-3.19.0-x86_64.iso
-qm create 9020 --memory 2048 --core 2 --name alpine --net0 virtio,bridge=vmbr1
-qm importdisk 9020 alpine-virt-3.19.0-x86_64.iso disks
-qm set 9020 --scsihw virtio-scsi-pci --scsi0 disks:9020/vm-9020-disk-0.raw
+qm create 9030 --memory 2048 --core 2 --name alpine --net0 virtio,bridge=vmbr1
+qm importdisk 9030 alpine-virt-3.19.0-x86_64.iso disks
+qm set 9030 --scsihw virtio-scsi-pci --scsi0 disks:9030/vm-9030-disk-0.raw
 sleep 10 # Give the disk time to mount
-virt-customize -a /mnt/pve/disks/images/9020/vm-9020-disk-0.raw --install qemu-guest-agent
-qm set 9020 --boot c --bootdisk scsi0 
-qm set 9020 --ide2 disks:cloudinit
-qm set 9020 --serial0 socket --vga serial0
-qm set 9020 --agent enabled=1
-qm set 9020 --ipconfig0 ip=dhcp
-qm set 9020 --sshkey ~/.ssh/id_rsa.pub
-qm set 9020 --tags alpine
-qm template 9020
+virt-customize -a /mnt/pve/disks/images/9030/vm-9030-disk-0.raw --install qemu-guest-agent
+qm set 9030 --boot c --bootdisk scsi0 
+qm set 9030 --ide2 disks:cloudinit
+qm set 9030 --serial0 socket --vga serial0
+qm set 9030 --agent enabled=1
+qm set 9030 --ipconfig0 ip=dhcp
+qm set 9030 --sshkey ~/.ssh/id_rsa.pub
+qm set 9030 --tags alpine
+qm template 9030
 rm alpine-virt-3.19.0-x86_64.iso
