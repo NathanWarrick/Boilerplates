8f0a696b2422a14cb3f7cb6933c8d18d0c70f8d2
NathanWarrick <nathan@wimmerait.com.au>
2023-12-13 12:27:53 +1100

    ffixed update-all

--- proxmox/cloud-images/update-all.sh
+++ proxmox/cloud-images/update-all.sh
@@ -1,5 +1,5 @@
 bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Boilerplates/main/proxmox/cloud-images/alpine/update-templates.sh)"
-bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Boilerplates/main/proxmox/cloud-images/centos/update-images.sh)"
+bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Boilerplates/main/proxmox/cloud-images/centos/update-templates.sh)"
 bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Boilerplates/main/proxmox/cloud-images/debian/update-templates.sh)"
 bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Boilerplates/main/proxmox/cloud-images/fedora/update-templates.sh)"
 bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Boilerplates/main/proxmox/cloud-images/opensuse/update-templates.sh)"
