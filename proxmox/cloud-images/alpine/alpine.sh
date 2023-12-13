9d3922f9f43860beb3774cec8b650ef4c705f6b8
NathanWarrick <nathan@wimmerait.com.au>
2023-12-13 17:56:34 +1100

    testing 22.04 minimal

--- proxmox/cloud-images/alpine/alpine.sh
+++ proxmox/cloud-images/alpine/alpine.sh
@@ -1,7 +1,7 @@
 qm destroy 9030
 
 wget -c https://dl-cdn.alpinelinux.org/alpine/v3.19/releases/x86_64/alpine-virt-3.19.0-x86_64.iso
-virt-customize -a alpine-virt-3.19.0-x86_64.iso --install qemu-guest-agent,nano
+virt-customize -a alpine-virt-3.19.0-x86_64.iso --install qemu-guest-agent,nano #TODO Virt-Customise isn't working
 
 qm create 9030 --memory 2048 --core 2 --name alpine --net0 virtio,bridge=vmbr1
 qm importdisk 9030 alpine-virt-3.19.0-x86_64.iso disks
--- proxmox/cloud-images/ubuntu/update-templates.sh
+++ proxmox/cloud-images/ubuntu/update-templates.sh
@@ -1,10 +1,10 @@
-bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Proxmox/main/cloud-images/ubuntu/18.04-LTS.sh)"
-bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Proxmox/main/cloud-images/ubuntu/18.04-LTS-Minimal.sh)"
+bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Boilerplates/main/cloud-images/ubuntu/18.04-LTS.sh)"
+bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Boilerplates/main/cloud-images/ubuntu/18.04-LTS-Minimal.sh)"
 
-bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Proxmox/main/cloud-images/ubuntu/20.04-LTS.sh)"
-bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Proxmox/main/cloud-images/ubuntu/20.04-LTS-Minimal.sh)"
+bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Boilerplates/main/cloud-images/ubuntu/20.04-LTS.sh)"
+bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Boilerplates/main/cloud-images/ubuntu/20.04-LTS-Minimal.sh)"
 
-bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Proxmox/main/cloud-images/ubuntu/22.04-LTS.sh)"
-bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Proxmox/main/cloud-images/ubuntu/22.04-LTS-Minimal.sh)"
+bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Boilerplates/main/cloud-images/ubuntu/22.04-LTS.sh)"
+bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Boilerplates/main/proxmox/cloud-images/ubuntu/22.04-LTS-Minimal.sh)"
 
-bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Proxmox/main/cloud-images/ubuntu/24.04-LTS.sh)"
+bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Boilerplates/main/cloud-images/ubuntu/24.04-LTS.sh)"
