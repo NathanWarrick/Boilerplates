c89ce42a6ed2b73839400f0c43e4e9f138a75337
NathanWarrick <42045377+NathanWarrick@users.noreply.github.com>
2023-12-14 12:57:01 +1100

    Create update-main.sh

---
+++ proxmox/cloud-images/update-main.sh
@@ -0,0 +1,4 @@
+bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Boilerplates/main/proxmox/cloud-images/ubuntu/22.04-LTS-Minimal.sh)"
+bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Boilerplates/main/proxmox/cloud-images/ubuntu/22.04-LTS.sh)"
+bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Boilerplates/main/proxmox/cloud-images/debian/debian-12.sh)"
+bash -c "$(wget -qLO - https://raw.githubusercontent.com/NathanWarrick/Boilerplates/main/proxmox/cloud-images/fedora/fedora-39.sh)"
