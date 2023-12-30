44224192cf5adb9a37d74b75ae099e8881ab6303
NathanWarrick <nathan@wimmerait.com.au>
2023-12-30 18:05:32 +1100

    added unzip

--- proxmox/cloud-images/fedora/coreos.sh
+++ proxmox/cloud-images/fedora/coreos.sh
@@ -1,14 +1,14 @@
 qm destroy 9020
 
-#wget -c https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/39.20231119.3.0/x86_64/fedora-coreos-39.20231119.3.0-live.x86_64.iso
 wget -c https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/39.20231119.3.0/x86_64/fedora-coreos-39.20231119.3.0-qemu.x86_64.qcow2.xz
+unxz fedora-coreos-39.20231119.3.0-qemu.x86_64.qcow2.xz
 
-virt-customize -a fedora-coreos-39.20231119.3.0-qemu.x86_64.qcow2.xz --install qemu-guest-agent,nano
+virt-customize -a fedora-coreos-39.20231119.3.0-qemu.x86_64.qcow2 --install qemu-guest-agent,nano
 sleep 2
-virt-sysprep --operations machine-id -a fedora-coreos-39.20231119.3.0-qemu.x86_64.qcow2.xz
+virt-sysprep --operations machine-id -a fedora-coreos-39.20231119.3.0-qemu.x86_64.qcow2
 
 qm create 9020 --memory 2048 --core 2 --name fedora-coreos--$(date +%d-%m-%Y) --net0 virtio,bridge=vmbr1  --pool Templates
-qm importdisk 9020 fedora-coreos-39.20231119.3.0-qemu.x86_64.qcow2.xz disks
+qm importdisk 9020 fedora-coreos-39.20231119.3.0-qemu.x86_64.qcow2 disks
 sleep 5
 qm set 9020 --scsihw virtio-scsi-pci --scsi0 disks:9020/vm-9020-disk-0.raw
 qm disk move 9020 scsi0 disks --format qcow2 --delete
@@ -22,4 +22,4 @@ qm set 9020 --sshkey ~/.ssh/id_rsa.pub
 qm set 9020 --tags fedora
 qm template 9020
 
-rm fedora-coreos-39.20231119.3.0-qemu.x86_64.qcow2.xz
+rm fedora-coreos-39.20231119.3.0-qemu.x86_64.qcow2
